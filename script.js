bf160e3839b88fa23c72c68e0511fa1cc6afdb1b
jbsainteve <jbsainteve@yahoo.fr>
2022-10-05 09:52:10 +0200

    correction bug event

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const mouse = {
     y: undefined
 };
 
-window.addEventListener("mousemove", function(e) {
+window.addEventListener("mousemove", function(event) {
     mouse.x = event.x;
     mouse.y = event.y;
 });
